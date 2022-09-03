fa2aa3f2cba63c09b02467b4d88147718d579332
TheEinfachFelix <the.einfach.felix@gmail.com>
2022-09-03 20:34:00 +0200

    Pixel detektion and more

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ This should work for all Versions. Tested only for 1.12.2
 **Limitations:** You can only brut force up to 10 character. Also the script takes it time so that the mod can keep up.
 
 pip install keyboard
+pip install pyautogui
+pip install Pillow --upgrade
+
+Please do not play in fullscreen mode! Window mode only. Press F11 to switch. Task bar at the bottom please. If the bar is standart or double size does not matter This will automatically scale to your display.
+
+Also the output of the combination might only be in the area of the true combination.
--- pwcracker.py
+++ pwcracker.py
@@ -1,24 +1,51 @@
 from time import sleep
-import keyboard
+from datetime import datetime
+import keyboard #you have to install this
+import pyautogui #this too
+
+
 
 toggle_key = 'z'
 exit_key = 'Escape'
-max_value = 299
+max_value = 1500
+start_time = datetime.now()
+
+
+
+
+
+def pixel_check():
+    img = pyautogui.screenshot() # makes Screenshot. Due to performance it does not get stored
+    pix = img.load()
+    img_size = img.size # some math to scale this to your diaplay
+    faktor_x = 800/1920
+    faktor_y = 511/1080
+    exact_x = int(img_size[0]*faktor_x)
+    exact_y = int(img_size[1]*faktor_y)
+    if (pix[exact_x,exact_y][0]+pix[exact_x,exact_y][1]+pix[exact_x,exact_y][2])/3 == 139: #checks color of pixel is correct
+        print("Pin is" + str(i-1))
+        print("correct combination found! stopping...")
+        print("Runtime: " + str(datetime.now() - start_time))
+        exit()
+
 
 print("Press " + toggle_key + " to start")
 while True:
-    if keyboard.is_pressed(exit_key):
+    if keyboard.is_pressed(exit_key): #exit while not running
             print("stopping")
+            print("Runtime: " + str(datetime.now() - start_time))
             exit()
     if keyboard.is_pressed(toggle_key):
         print("Running...")
-        if True: #Add pixle detektion
-            for i in range(max_value):
-                sleep(0.05)
-                if keyboard.is_pressed(exit_key):
-                    print("aborting")
-                    exit()
-                keyboard.write(str(i)) #writes the current number
-                for x  in range(len(str(i)) * 2): #deleting the written and some more 
-                    keyboard.write('\b')
-            print("done")
+        for i in range(max_value):
+            sleep(0.05)
+            if keyboard.is_pressed(exit_key): #exit while running
+                print("aborting")
+                print("Runtime: " + str(datetime.now() - start_time))
+                exit()
+            keyboard.write(str(i)) #writes the current number
+            pixel_check()
+            for x  in range(len(str(i)) * 2): #deleting the written and some more 
+                keyboard.write('\b')
+        print("done")
+        print("Runtime: " + str(datetime.now() - start_time))
