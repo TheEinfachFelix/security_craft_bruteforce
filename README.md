2445aec4605628726fc2b08a256b736c9bca99ac
TheEinfachFelix <the.einfach.felix@gmail.com>
2022-09-03 16:31:23 +0200

    smarter code bug fix and better coments

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ This Script can be used to bypass the key locks from the security craft Minecraf
 This should work for all Versions. Tested only for 1.12.2
 
 **Limitations:** You can only brut force up to 10 character. Also the script takes it time so that the mod can keep up.
+
+pip install keyboard
--- pwcracker.py
+++ pwcracker.py
@@ -5,18 +5,6 @@ toggle_key = 'z'
 exit_key = 'Escape'
 max_value = 299
 
-def backspaces(): #this funktion deletes the input, cause i cant use an additional loop there. Yes hardcoding is bad but this is the easiest and i dont want to spend 5h to fix it.(len(str(i)) * 2)
-    keyboard.write('\b')
-    keyboard.write('\b')
-    keyboard.write('\b')
-    keyboard.write('\b')
-    keyboard.write('\b')
-    keyboard.write('\b')
-    keyboard.write('\b')
-    keyboard.write('\b')
-    keyboard.write('\b')
-    keyboard.write('\b')
-
 print("Press " + toggle_key + " to start")
 while True:
     if keyboard.is_pressed(exit_key):
@@ -28,8 +16,9 @@ while True:
             for i in range(max_value):
                 sleep(0.05)
                 if keyboard.is_pressed(exit_key):
-                    print("exiting")
+                    print("aborting")
                     exit()
-                keyboard.write(str(i))
-                backspaces()
+                keyboard.write(str(i)) #writes the current number
+                for x  in range(len(str(i)) * 2): #deleting the written and some more 
+                    keyboard.write('\b')
             print("done")
