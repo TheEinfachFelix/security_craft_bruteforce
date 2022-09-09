1cea41d0ffc41ae9a420e07894db2b6d9c7c5d8c
TheEinfachFelix <the.einfach.felix@gmail.com>
2022-09-09 22:15:04 +0200

    big fix

--- pwcracker.py
+++ pwcracker.py
@@ -15,9 +15,9 @@ start_time = datetime.now()
 
 faktor_x = 800/1920
 faktor_y = 511/1080
-
 once = True
 running = True
+i = 0
 
 
 def pixel_check():
@@ -25,8 +25,10 @@ def pixel_check():
     global once
     global exact_x
     global exact_y
+    global i
     img = pyautogui.screenshot() # makes Screenshot. Due to performance it does not get stored
     pix = img.load()
+    print(i)
     img_size = img.size # some math to scales coards to your display
     if once == True: # Run this only once to set the correct factors to your display size
         exact_x = int(img_size[0]*faktor_x)
@@ -40,6 +42,7 @@ def pixel_check():
 
 def main():
     global running
+    global i
     print("Press " + toggle_key + " to start")
     while running:
         if keyboard.is_pressed(exit_key): #exit while not running
